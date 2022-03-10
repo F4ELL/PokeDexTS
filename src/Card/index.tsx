b2670e42d7a68125b3d48070d7d5b27bf5e81858
F4ELL <ferreiraperes1234@gmail.com>
2022-03-10 16:43:14 -0300

    feat: create full card

--- src/Card/index.tsx
+++ src/Card/index.tsx
@@ -23,8 +23,28 @@ const Card: React.FC<TCardPoke> = ({item, index}) => {
                         <span>{item?.items.types[0].type.name}</span>
                     </div>
                 </div>
+                <div className="card--image">
+                    <img src={item?.items.sprites.other.home.front_default} alt={item?.title} />    
+                </div>
                 <div className="card--bottom">
+                    <div className="card--status">
+                        <h3>Status</h3>
+                        <ul>
+                            <li>HP: {item?.items.stats[0].base_stat}</li>
+                            <li>Ataque: {item?.items.stats[1].base_stat}</li>
+                            <li>Defesa: {item?.items.stats[2].base_stat}</li>
+                            <li>Velocidade: {item?.items.stats[5].base_stat}</li>
+                        </ul>
                     </div>
+                    <div className="card--skills">
+                        <h3>Habilidades</h3>
+                        <ul>
+                            {item?.items.abilities.map((item, key) => (
+                                <li key={key}>{item.ability.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
                 
             </div>
         </StyledCard>
